bb3efb013015ba0ba5862019e3094c3ab2ec5e5c
aaronium112 <aaron@sphericalwave.com>
2020-08-19 18:22:30 -0300

    Initial Commit

---
+++ ScrollDate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrollDate
+//
+//  Created by Aaron Anthony on 2020-08-19.
+//  Copyright © 2020 SphericalWaveSoftware. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
